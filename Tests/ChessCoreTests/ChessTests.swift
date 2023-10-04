4e87755254119532be2f49ac3ba5e435bae6eb12
Ilias Karim <iliasyoga@outlook.com>
2023-10-04 18:14:07 -0400

    Update ChessTests.swift

--- Tests/ChessCoreTests/ChessTests.swift
+++ Tests/ChessCoreTests/ChessTests.swift
@@ -2,8 +2,27 @@ import XCTest
 @testable import ChessCore
 
 final class ChessTests: XCTestCase {
-  func testPromotion() throws {
-    var game = Game(board: [Square(file: .a, rank: .seven): Piece(color: .white, figure: .pawn)])
+  private var game = Game(board: [Square(file: .a, rank: .seven): Piece(color: .white, figure: .pawn)])
+
+  func testPromotionToBishop() throws {
+    print(game)
+    try game.move("a8B")
+    print(game)
+  }
+
+  func testPromotionToKnight() throws {
+    print(game)
+    try game.move("a8N")
+    print(game)
+  }
+
+  func testPromotionToRook() throws {
+    print(game)
+    try game.move("a8R")
+    print(game)
+  }
+
+  func testPromotionToQueen() throws {
     print(game)
     try game.move("a8Q")
     print(game)
